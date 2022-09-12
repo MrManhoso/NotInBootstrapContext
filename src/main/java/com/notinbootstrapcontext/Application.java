faff148895404c79114b141f8edb2526e10fed43
Erik Jonsson <MrManhoso>
2022-09-12 12:50:23 +0000

    Initial commit

---
+++ src/main/java/com/notinbootstrapcontext/Application.java
@@ -0,0 +1,9 @@
+package com.notinbootstrapcontext;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
